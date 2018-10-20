637001cccc3ba3142612d2fedc0b40e689973eb2
Ti <zttttty@TideMacBook-Air.local>
2018-10-19 21:00:07 -0400

    heroku setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_171146) do
+ActiveRecord::Schema.define(version: 2018_10_20_005849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,11 @@ ActiveRecord::Schema.define(version: 2018_10_19_171146) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
   create_table "users_and_courses", force: :cascade do |t|
